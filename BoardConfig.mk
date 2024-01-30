6cc41f55f566cbf8bf0222bd27a95217be74dbea
Georg Veichtlbauer <georg@vware.at>
2024-01-30 09:29:36 +0100

    Ginkgo: powerhint: Add DT2W
    Change-Id: I7d0dfe249798864a39afd4f339b692584faaf3ec
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,9 +72,6 @@ TARGET_USES_DISPLAY_RENDER_INTENTS := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
-# DT2W
-TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
-
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/fs/config.fs
 
