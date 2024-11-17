31d4aa8be6afdec28a4b78acde0e72fae0ae66de
ZorEl212 <yabsirad212@gmail.com>
2024-11-17 23:27:23 +0530

    Ginkgo: Guard EROFS flags to use EROFS on GApps builds only
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -102,7 +102,11 @@ LOCAL_MODULE       := fstab.qcom_ramdisk
 LOCAL_MODULE_STEM  := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
+ifeq ($(WITH_GMS), true)
+LOCAL_SRC_FILES := etc/fstab.qcom-erofs
+else
 LOCAL_SRC_FILES    := etc/fstab.qcom
+endif
 LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
 
@@ -110,7 +114,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.qcom
+ifeq ($(WITH_GMS), true)
+LOCAL_SRC_FILES := etc/fstab.qcom-erofs
+else
+LOCAL_SRC_FILES := etc/fstab.qcom
+endif
 LOCAL_REQUIRED_MODULES := fstab.qcom_ramdisk
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
